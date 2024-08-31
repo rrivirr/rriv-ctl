0f76d85c400da73b9571f323b3664864832c793d
Zaven Arra <zaven@pop-os.localdomain>
2024-08-30 18:26:52 -0700

    feat: implement watch mode for outputing realtime sensor data to files

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { description, version } from '../package.json'
 import { SerialPort, ReadlineParser } from 'serialport';
 import * as fs from 'fs';
 import moment from 'moment'
+import path from 'path'
 
 let cli = new Command()
 
@@ -19,28 +20,57 @@ cli
     console.log('test command called')
   })
 
-function readSerialUntilQuit(path: string, file: string){
+function readSerialUntilQuit(serialPath: string, file: string) {
 
-  console.log(`Watching output and logging sensor data to ${file}\n`)
+  const homedir = require('os').homedir();
+
+  const dir = path.join(homedir, ".rriv", "watch");
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+
+  const logPath = path.join(dir, file)
+  const dirPath = logPath.substring(0, logPath.lastIndexOf("/"));
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+
+  console.log(`Watching output and logging sensor data to ${logPath}\n`)
+
+
 
   const parser = new ReadlineParser({
     delimiter: '\n',
     includeDelimiter: false
   })
   const serialPort = new SerialPort({
-    path, //'/dev/serial/by-id/usb-RRIV_RRIV_Data_Logger__rriv-if00',
+    path: serialPath, //'/dev/serial/by-id/usb-RRIV_RRIV_Data_Logger__rriv-if00',
     baudRate: 57600,
   });
-  serialPort.pipe(parser)
+  serialPort.write("{\"object\":\"datalogger\", \"action\":\"set_mode\", \"mode\":\"quiet\"}\n");
+  serialPort.drain(() => {
+
+    setTimeout(() => {
+      serialPort.flush();
+      serialPort.pipe(parser)
+      serialPort.write("{\"object\":\"datalogger\", \"action\":\"set_mode\", \"mode\":\"watch\"}\n");
+    }, 1000);
+  });
 
   parser.on('data', function (data: String) {
     console.log(data);
+    if (data[0] == '{') {
+      // skip this line
+      return;
+    }
 
-
-
-    fs.writeFileSync(file, moment().format() + ',' + data.toString() + '\n', {
-      flag: "a"
-    })
+    fs.writeFileSync(
+      logPath,
+      moment().format() + ',' + data.toString() + '\n',
+      {
+        flag: "a"
+      })
   });
 
 }
@@ -51,30 +81,40 @@ cli
   .description('watch data output and log to a file')
   .option('-f, --file <file>', 'name of a file to output sensor data to')
   .option('-p, --path <serial_path>', 'serial path of the RRIV device')
+  .option('--project <project>', 'a project name for organizing watch files')
   .action((options) => {
-    console.log(options);
-    // console.log(name);
-    const file = options.file;
 
-    if(!options.path){
+    let file = options.file
+    let project = options.project
+
+    if (!project) {
+      project = "rriv";
+    }
+
+    if (!file) {
+      // file = path.join(project, project + "_" + moment().format('YYYY-MM-DDTHH:mm') + "_watch.txt")
+      file = project + "_" + moment().format('YYYY-MM-DDTHH:mm') + "_watch.txt"
+    }
+
+    if (!options.path) {
       SerialPort.list().then((list) => {
 
         // detect the serial port
         let serialPortPath = "";
-        for( const pathItem of list ){
-          if( pathItem.productId && pathItem.pnpId?.includes('rriv')){
+        for (const pathItem of list) {
+          if (pathItem.productId && pathItem.pnpId?.includes('rriv')) {
             console.log(`Found a RRIV device ${pathItem.pnpId}`)
             console.log(`Connecting to it at ${pathItem.path}`)
             serialPortPath = pathItem.path
           }
         }
-        if(serialPortPath === ""){
+        if (serialPortPath === "") {
           console.log("No RRIV device found")
           console.log("Try using -p <path> to specify the path to the RRIV serial device")
         }
 
         readSerialUntilQuit(serialPortPath, file);
-        
+
       })
     } else {
       readSerialUntilQuit(options.path, file);
