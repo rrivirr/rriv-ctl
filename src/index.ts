8003002de71501bb0938586bbcd5e2d45ac4b720
Zaven Arra <zaven.arra@gmail.com>
2025-01-25 16:04:45 -0800

    feat: added calibrate command

--- src/index.ts
+++ src/index.ts
@@ -328,10 +328,10 @@ cli
   cli
   .command('calibrate')
   .addArgument(new Argument('<object>').choices(['sensor']))
-  .argument('<id>')
-  .argument('subcommand')
-  .argument('[point]')
-  .argument('[tag]')
+  .argument('<id>', 'The id of the sensor to calibrate.')
+  .addArgument(new Argument('<subcommand>').choices(['point, list, fit, or clear']))
+  .argument('[point]', 'A reference reading for the point command, float type')
+  // .argument('[tag]')
   .description('calibration commands')
   .action((object, id, subcommand, point, tag) => {
 
