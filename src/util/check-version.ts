b22a00cf17ade258ef4bc5ab6853e657f8a2f90e
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2025-08-12 15:35:59 +0100

    fix: fix check-version function

--- src/util/check-version.ts
+++ src/util/check-version.ts
@@ -24,7 +24,7 @@ export const checkVersion = async (source: Source = "preAction") => {
         if (answer) {
           const mergeResult = await exec(`git merge origin/${workingBranch}`);
           console.log(mergeResult.stdout);
-          if (result.stdout.includes("package.json")) {
+          if (mergeResult.stdout.includes("package.json")) {
             const npmResult = await exec(`npm i`);
             console.log(npmResult.stdout);
           }
