75c58562d46d501673c5615bedf0c13ff9706358
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2025-09-19 16:50:51 +0100

    fix: auth error messages

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,31 +10,47 @@ import { SignupDto } from "../../api/types.ts";
 import db from "../../db/db.ts";
 import { passwordPrompt, signupPrompt } from "../../prompts/auth.prompt.ts";
 import { JwtPayload } from "../../types.ts";
+import { italic } from "yoctocolors";
 
 export const login = async (email: string) => {
-  const { accessToken: existingAccessToken } = db.data;
-  const password = await passwordPrompt(false, false);
-  const { accessToken, expiresIn } = await loginApiCall({
-    username: email,
-    password,
-  });
+  try {
+    const { accessToken: existingAccessToken } = db.data;
+    const password = await passwordPrompt(false, false);
+    const { accessToken, expiresIn } = await loginApiCall({
+      username: email,
+      password,
+    });
 
-  if (existingAccessToken) {
-    const oldDecodedToken: JwtPayload = jwtDecode(existingAccessToken);
-    const newDecodedToken: JwtPayload = jwtDecode(accessToken);
+    if (existingAccessToken) {
+      const oldDecodedToken: JwtPayload = jwtDecode(existingAccessToken);
+      const newDecodedToken: JwtPayload = jwtDecode(accessToken);
 
-    if (oldDecodedToken.email !== newDecodedToken.email) {
-      // clear cached information
-      logout();
+      if (oldDecodedToken.email !== newDecodedToken.email) {
+        // clear cached information
+        logout();
+      }
     }
-  }
-  const now = new Date();
-  db.update((data) => {
-    data.accessToken = accessToken;
-    data.expirationTime = +now.setSeconds(now.getSeconds() + expiresIn);
-  });
+    const now = new Date();
+    db.update((data) => {
+      data.accessToken = accessToken;
+      data.expirationTime = +now.setSeconds(now.getSeconds() + expiresIn);
+    });
 
-  console.log("authentication successful");
+    console.log("authentication successful");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (
+      error?.response?.data?.error_description === "Account is not fully set up"
+    ) {
+      console.log(
+        `\nYou must verify your email address to log in.
+Please check your email and follow the verification link.
+To resend the verification email run the command ${italic(`rrivctl auth verify ${email}`)}`
+      );
+      return;
+    }
+    throw error;
+  }
 };
 
 export const logout = () => {
@@ -64,7 +80,9 @@ export const signup = async (body: Partial<Omit<SignupDto, "password">>) => {
   const signupBody = await signupPrompt(body);
 
   await signupApiCall({ ...signupBody, accessToken });
-  console.log("signup successful");
+  console.log(
+    "\nSignup complete. You must verify your email address to log in.\nPlease check your email and follow the verification link."
+  );
 };
 
 export const verify = async (email: string) => {
