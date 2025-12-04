bb793aed85bd9b22c6a35b45fc56f9996a8b0f38
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2025-12-05 00:24:29 +0100

    fix: binary debug attempt

--- src/infra/s3.ts
+++ src/infra/s3.ts
@@ -1,13 +1,13 @@
-import { S3 } from "@aws-sdk/client-s3";
+// import { S3 } from "@aws-sdk/client-s3";
 
-const s3Client = new S3({
-  forcePathStyle: false,
-  endpoint: process.env.SPACES_ENDPOINT!,
-  region: "us-east-1",
-  credentials: {
-    accessKeyId: process.env.SPACES_ACCESS_KEY!,
-    secretAccessKey: process.env.SPACES_SECRET_KEY!,
-  },
-});
+// const s3Client = new S3({
+//   forcePathStyle: false,
+//   endpoint: process.env.SPACES_ENDPOINT!,
+//   region: "us-east-1",
+//   credentials: {
+//     accessKeyId: process.env.SPACES_ACCESS_KEY!,
+//     secretAccessKey: process.env.SPACES_SECRET_KEY!,
+//   },
+// });
 
-export { s3Client };
+// export { s3Client };
