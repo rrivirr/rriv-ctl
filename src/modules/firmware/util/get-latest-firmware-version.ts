6edf9aab3bec3e746d274f8513d0ec0acb3d6a36
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-02-23 21:16:36 +0100

    fix: extend getLatestFirmwareVersion to support firmware and scripts repos

--- src/modules/firmware/util/get-latest-firmware-version.ts
+++ src/modules/firmware/util/get-latest-firmware-version.ts
@@ -1,11 +1,17 @@
 import { Octokit } from "@octokit/rest";
 
-export const getLatestFirmwareVersion = async () => {
+export const getLatestFirmwareVersion = async (diagnostic = false) => {
   console.log("looking up latest firmware...");
+  let repo: string;
+  if (diagnostic) {
+    repo = "rriv-scripts";
+  } else {
+    repo = "rriv-firmware";
+  }
   const octokit = new Octokit();
   const release = await octokit.repos.getLatestRelease({
     owner: "rrivirr",
-    repo: "rriv-firmware",
+    repo,
   });
   return release.data.tag_name;
 };
