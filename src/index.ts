dac241342807f5114d60d3701774d5714dde5ad7
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-02-26 14:58:45 +0100

    fix: rework auto complete check

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,49 @@
 #!/usr/bin/env node
 import "dotenv/config";
+import path from "node:path";
+import { readFileSync } from "node:fs";
 import { errorHandler } from "./util/error-handler.ts";
 import initAutoComplete from "./auto-complete.ts";
 import { getAutoCompleteTree } from "./auto-complete-tree.ts";
 import cli from "./cli.ts";
 import db from "./db/db.ts";
+import { logAsDebug } from "./util/debug-logger.ts";
 
 const tree = getAutoCompleteTree(cli);
 const completion = initAutoComplete(tree);
 
-const { autoCompleteSetup } = db.data;
 const setupAutoComplete = () => {
+  const { autoCompleteSetup } = db.data;
   if (!autoCompleteSetup) {
-    db.update((data) => {
-      data.autoCompleteSetup = true;
-    });
-    completion.setupShellInitFile(); // calls process.exit
+    const shell = process.env.SHELL!;
+    let configFilePath = ``;
+
+    if (shell.match(/bash/)) {
+      configFilePath = path.join(process.env.HOME!, ".bashrc");
+    } else if (shell.match(/zsh/)) {
+      configFilePath = path.join(process.env.HOME!, ".zshrc");
+    }
+
+    if (configFilePath) {
+      const configData = readFileSync(configFilePath, "utf-8");
+      if (configData.includes("begin rrivctlv2 completion")) {
+        db.update((data) => {
+          data.autoCompleteSetup = true;
+        });
+      } else {
+        completion.setupShellInitFile(); // calls process.exit
+      }
+    } else {
+      logAsDebug(process.env.SHELL, " found");
+    }
   }
 };
 
-if (process.argv.includes("--setup-completion")) {
-  setupAutoComplete();
-} else if (!process.argv.includes("--completion")) {
+if (!process.argv.includes("--completion")) {
   cli
     .parseAsync()
     .then(setupAutoComplete)
     .catch((error) => {
-      errorHandler({ error, exit: autoCompleteSetup });
-      setupAutoComplete();
+      errorHandler({ error, exit: true });
     });
 }
