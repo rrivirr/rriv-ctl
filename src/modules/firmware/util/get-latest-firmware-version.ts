d9ae300711681553700fc334d42fdff0de5c631f
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-01 00:19:49 +0100

    chore: reposition console.log

--- src/modules/firmware/util/get-latest-firmware-version.ts
+++ src/modules/firmware/util/get-latest-firmware-version.ts
@@ -1,13 +1,14 @@
 import { Octokit } from "@octokit/rest";
 
 export const getLatestFirmwareVersion = async (diagnostic = false) => {
-  console.log("looking up latest firmware...");
   let repo: string;
   if (diagnostic) {
     repo = "rriv-scripts";
   } else {
     repo = "rriv-firmware";
   }
+
+  console.log(`looking up latest ${repo} firmware...`);
   const octokit = new Octokit();
   const release = await octokit.repos.getLatestRelease({
     owner: "rrivirr",
