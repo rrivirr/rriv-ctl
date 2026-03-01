6fd1e1dd51c9aa6ed740f2757138afe66e503ba4
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-01 01:05:52 +0100

    feat: clear eeprom command

--- src/commands/firmware/action.ts
+++ src/commands/firmware/action.ts
@@ -1,5 +1,5 @@
 import { listFirmwareHistory } from "../../modules/firmware/firmware.service.ts";
-import { flashFirmware } from "../../modules/firmware/flash.ts";
+import { clearEeprom, flashFirmware } from "../../modules/firmware/flash.ts";
 import { probeDebug } from "../../modules/firmware/probe-debug.ts";
 import { runDiagnostics } from "../../modules/firmware/run-diagnostics.ts";
 
@@ -17,4 +17,8 @@ export const listFirmwareHistoryAction = async (serialNumber?: string) => {
 
 export const diagnosticAction = async (firmwareVersion?: string) => {
   await runDiagnostics(firmwareVersion);
+};
+
+export const clearEepromAction = async () => {
+  await clearEeprom();
 };
