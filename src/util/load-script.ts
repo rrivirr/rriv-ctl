961d9716dc33b119fa45401e361b76a062b21e7c
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-05 16:17:06 +0100

    fix: check if script is in scripts folder before attempting to load

--- src/util/load-script.ts
+++ src/util/load-script.ts
@@ -1,9 +1,18 @@
-import fs from "node:fs";
+import fs, { existsSync } from "node:fs";
 import path from "node:path";
 import { pipeline } from "node:stream/promises";
+import { getRrivCtlScriptsDir } from "./paths.ts";
 
 export const loadScript = async (script: string, fileName: string) => {
-  const file = fs.createWriteStream(fileName);
-  await pipeline(fs.createReadStream(path.join(__dirname, `${script}`)), file);
-  fs.chmodSync(fileName, 0o755);
+  const scriptDestination = `${getRrivCtlScriptsDir()}/${fileName}`;
+  const fileExists = existsSync(scriptDestination);
+  if (!fileExists) {
+    const file = fs.createWriteStream(scriptDestination);
+    await pipeline(
+      fs.createReadStream(path.join(__dirname, `${script}`)),
+      file,
+    );
+    fs.chmodSync(scriptDestination, 0o755);
+  }
+  return scriptDestination;
 };
