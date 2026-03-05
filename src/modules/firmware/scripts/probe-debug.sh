a1bd57a2b0e1b5c3973b9c10221b567d967c185b
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-05 16:13:10 +0100

    fix: check if firmware exists

--- src/modules/firmware/scripts/probe-debug.sh
+++ src/modules/firmware/scripts/probe-debug.sh
@@ -3,10 +3,14 @@ set -e
 
 dirPath=$1
 firmware_version=$2
+file_path=$dirPath/rriv-firmware-debug-$firmware_version.elf
 
-curl --progress-bar --proto '=https' --tlsv1.2 -fSLo $dirPath/rriv-firmware-debug.elf https://github.com/rrivirr/rriv-firmware/releases/download/$firmware_version/rriv-firmware-debug.elf
+if [ ! -e "$file_path" ]; then
+   echo "Downloading firmware"
+   curl --progress-bar --proto '=https' --tlsv1.2 -fSLo $file_path https://github.com/rrivirr/rriv-firmware/releases/download/$firmware_version/rriv-firmware-debug.elf
+fi
 
-probe-rs attach $dirPath/rriv-firmware-debug.elf \
+probe-rs attach $file_path \
         --chip STM32F103RE  \
         --protocol swd \
         --allow-erase-all \
