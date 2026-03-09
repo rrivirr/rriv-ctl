de0d5b94cd4ef3980d43d736105743bb1b1a59fd
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-09 22:59:11 +0100

    feat: add logs command

--- src/modules/device/device.service.ts
+++ src/modules/device/device.service.ts
@@ -2,6 +2,8 @@ import Table from "cli-table3";
 import {
   unbindDevice as unbindDeviceApiCall,
   getDevices as getDevicesApiCall,
+  getLogs,
+  createLog,
 } from "../../api/device.ts";
 import db from "../../db/db.ts";
 import { pronounce } from "../../util/console-log.ts";
@@ -69,4 +71,48 @@ export const listDevices = async (all?: boolean) => {
   } else {
     await listContextDevices();
   }
+};
+
+export const listLogs = async (identifier?: string) => {
+  const {
+    device: { serialNumber: connectedSerialNumber },
+  } = getActiveUser();
+  if (!connectedSerialNumber && !identifier) {
+    throw new Error("serial number is required");
+  }
+
+  const logs = await getLogs({
+    identifier: identifier || connectedSerialNumber,
+  });
+
+  if (logs.length) {
+    const table = new Table({
+      head: ["log", "createdAt", "createdBy"],
+      wordWrap: true,
+      wrapOnWordBoundary: false,
+    });
+    for (const {
+      log,
+      createdAt,
+      Creator: { firstName, lastName },
+    } of logs) {
+      table.push([log, createdAt, `${firstName} ${lastName}`]);
+    }
+    console.log(table.toString());
+  } else {
+    console.log("no logs found");
+  }
+};
+
+export const addLog = async (log: string, identifier?: string) => {
+  const {
+    device: { serialNumber: connectedSerialNumber },
+  } = getActiveUser();
+
+  if (!connectedSerialNumber && !identifier) {
+    throw new Error("serial number is required");
+  }
+
+  await createLog({ log, identifier: identifier || connectedSerialNumber });
+  console.log("successful");
 };
