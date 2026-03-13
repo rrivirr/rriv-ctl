37a46869a576d96e65ad2a90a16557e712d61986
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-13 13:13:29 +0100

    fix: load script without checking for duplicate

--- src/util/load-script.ts
+++ src/util/load-script.ts
@@ -1,18 +1,13 @@
-import fs, { existsSync } from "node:fs";
+import fs from "node:fs";
 import path from "node:path";
 import { pipeline } from "node:stream/promises";
 import { getRrivCtlScriptsDir } from "./paths.ts";
 
 export const loadScript = async (script: string, fileName: string) => {
   const scriptDestination = `${getRrivCtlScriptsDir()}/${fileName}`;
-  const fileExists = existsSync(scriptDestination);
-  if (!fileExists) {
-    const file = fs.createWriteStream(scriptDestination);
-    await pipeline(
-      fs.createReadStream(path.join(__dirname, `${script}`)),
-      file,
-    );
-    fs.chmodSync(scriptDestination, 0o755);
-  }
+  const file = fs.createWriteStream(scriptDestination);
+  await pipeline(fs.createReadStream(path.join(__dirname, `${script}`)), file);
+  fs.chmodSync(scriptDestination, 0o755);
+
   return scriptDestination;
 };
