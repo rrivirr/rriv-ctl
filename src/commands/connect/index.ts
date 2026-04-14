09a3de1a921fa0a2c35e066f5a2e951a26977af5
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-04-14 12:45:11 +0100

    fix: make device unique name the default assigned device name

--- src/commands/connect/index.ts
+++ src/commands/connect/index.ts
@@ -4,10 +4,6 @@ import { connectAction } from "./action.ts";
 export const makeConnectCommand = (cli: Command) => {
   cli
     .command("connect")
-    .option(
-      "-a, --assigned-device-name <assigned device name>",
-      "name to assign to a device in current context",
-    )
     .option("-p, --path <serial path>", "serial path of the RRIV device")
     .option("-i, --interactive-mode", "set device to use interactive mode")
     .action(connectAction);
