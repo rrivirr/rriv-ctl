c6dc7d933e28303de5f4b7fc81c3a862812cb88a
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-05-12 15:26:56 +0100

    fix: remove all sensors

--- src/commands/remove/action.ts
+++ src/commands/remove/action.ts
@@ -2,7 +2,34 @@ import { unbindDevice } from "../../modules/device/device.service.ts";
 import { sendCommands } from "../../infra/send-commands.ts";
 import { oraPromise } from "../../util/ora-promise.ts";
 
-export const removeAction = async (object: string, id: string) => {
+export const removeAction = async (
+  object: string,
+  id: string,
+  options: any,
+) => {
+  const { all } = options;
+  if (object === "sensor" && all) {
+    const [sensors] = await oraPromise(() =>
+      sendCommands(
+        [JSON.stringify({ object: "sensor", action: "list" })],
+        false,
+      ),
+    );
+
+    if (sensors?.sensors?.length) {
+      await oraPromise(() =>
+        sendCommands(
+          sensors.sensors.map((s: any) =>
+            JSON.stringify({ object: "sensor", action: "remove", id: s.id }),
+          ),
+        ),
+      );
+    }
+    return;
+  }
+  if (!id) {
+    throw new Error("argument required");
+  }
   if (object === "device") {
     await oraPromise(() => unbindDevice(id));
     return;
